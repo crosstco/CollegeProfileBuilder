5dd4e0ee2f03916306cb79983c85bdc1f259237b
Colin <Colin@Administrators-MacBook-Air.local>
2016-01-21 10:09:10 -0600

    Built storyboard. Implemented necessary protocol functions. Created array of 3 base colleges

---
+++ CollegeProfileBuilder/College.swift
@@ -0,0 +1,33 @@
+//
+//  College.swift
+//  CollegeProfileBuilder
+//
+//  Created by Colin on 1/20/16.
+//  Copyright © 2016 ccross. All rights reserved.
+//
+
+import UIKit
+
+class College: NSObject {
+    
+    var name = ""
+    var location = ""
+    var numStudents = 0
+    var image = UIImage(named: "default")
+    
+    
+    init(name : String, location : String, numStudents : Int, image : UIImage) {
+        super.init()
+        self.name = name
+        self.location = location
+        self.numStudents = numStudents
+        self.image = image
+    }
+    
+    init(name: String, location : String) {
+        super.init()
+        self.name = name
+        self.location = location
+    }
+    
+}
--- CollegeProfileBuilder/ViewController.swift
+++ CollegeProfileBuilder/ViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ViewController.swift
 //  CollegeProfileBuilder
 //
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    
+    var colleges : [College] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        colleges.append(College(name: "Purdue University", location: "West Lafayette, IN", numStudents: 40000, image: UIImage(named: "default")!))
+        
+            colleges.append(College(name: "University of Illinois", location: "Urbana-Champaign, IL", numStudents: 40000, image: UIImage(named: "default")!))
+        
+        colleges.append(College(name: "University of Wisconsin - Madison", location: "West Lafayette, IN", numStudents: 40000, image: UIImage(named: "default")!))
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    //REQUIRED FUNCTIONS for TableView Protocols
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell { //Sends data to cell
+        
+        //create cell variable for tableView
+        let collegeCell = tableView.dequeueReusableCellWithIdentifier("collegeCell", forIndexPath: indexPath)
+        
+        //sets properties of the cell
+        collegeCell.textLabel?.text = colleges[indexPath.row].name
+        collegeCell.detailTextLabel?.text = colleges[indexPath.row].location
+        
+        return collegeCell
     }
-
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int { //sets num rows in table
+        return colleges.count
+    }
 
 }
 
